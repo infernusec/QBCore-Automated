4e0ee75a64ac4a7292829f4be13e92ca6c9f7c87
Infernus <2491403+infernusec@users.noreply.github.com>
2023-05-22 22:44:04 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,68 +13,15 @@ Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 
-# Bold
-BBlack='\033[1;30m'       # Black
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
-BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
-BWhite='\033[1;37m'       # White
-
-# Underline
-UBlack='\033[4;30m'       # Black
-URed='\033[4;31m'         # Red
-UGreen='\033[4;32m'       # Green
-UYellow='\033[4;33m'      # Yellow
-UBlue='\033[4;34m'        # Blue
-UPurple='\033[4;35m'      # Purple
-UCyan='\033[4;36m'        # Cyan
-UWhite='\033[4;37m'       # White
-
-# Background
-On_Black='\033[40m'       # Black
-On_Red='\033[41m'         # Red
-On_Green='\033[42m'       # Green
-On_Yellow='\033[43m'      # Yellow
-On_Blue='\033[44m'        # Blue
-On_Purple='\033[45m'      # Purple
-On_Cyan='\033[46m'        # Cyan
-On_White='\033[47m'       # White
-
-# High Intensity
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-
-# Bold High Intensity
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
-
-# High Intensity backgrounds
-On_IBlack='\033[0;100m'   # Black
-On_IRed='\033[0;101m'     # Red
-On_IGreen='\033[0;102m'   # Green
-On_IYellow='\033[0;103m'  # Yellow
-On_IBlue='\033[0;104m'    # Blue
-On_IPurple='\033[0;105m'  # Purple
-On_ICyan='\033[0;106m'    # Cyan
-On_IWhite='\033[0;107m'   # White
-
 UNIX_USER="qbcfivem"
 DB_USER="fivem"
+FIVEM_FRAMEWORK="qbcore"
+
+#if [ $FIVEM_FRAMEWORK == "esx" ]; then
+    # https://github.com/esx-framework/esx_core.git
+#elif [ $FIVEM_FRAMEWORK == 'qbcore' ]; then
+
+#fi
 
 echo -e "${Cyan}[INFO]:${White} QBCore Automated Installer"
 echo -e "${Cyan}[INFO]:${White} Removing old packages & files & users.."
@@ -116,10 +63,13 @@ dnf install -y nano git zip unzip firewalld tar xz 1>/dev/null
 update_repos
 echo -e "${Cyan}[INFO]:${White} Adding user [$UNIX_USER].."
 adduser "$UNIX_USER" 1>/dev/null
-echo -e "$UNIX_USER_PASS\n$UNIX_USER_PASS" | passwd "$UNIX_USER" 1>/dev/null
+echo -e "$UNIX_USER_PASS\n$UNIX_USER_PASS" | passwd "$UNIX_USER" &>/dev/null
 
 # Add user to sudo group
-usermod -a -G wheel $UNIX_USER 1>/dev/null
+# usermod -a -G wheel $UNIX_USER 1>/dev/null
+
+# new strict, allow to user run only systemctl command for specific service (such a qbcore)
+sed -i "/^# %wheel\s\+ALL=(ALL)\s\+NOPASSWD: ALL$/a $UNIX_USER ALL=(ALL) NOPASSWD: /bin/systemctl status qbcore, /bin/systemctl start qbcore, /bin/systemctl stop qbcore, /bin/systemctl restart qbcore, /bin/systemctl enable qbcore, /bin/systemctl disable qbcore, /bin/journalctl -u qbcore.service" /etc/sudoers
 
 echo -e "${Cyan}[INFO]:${White} Prepare server files.."
 mkdir -p "/home/$UNIX_USER/server"
@@ -144,8 +94,9 @@ gpgcheck = 1" > /etc/yum.repos.d/mariadb.repo
 update_repos
 
 echo -e "${Cyan}[INFO]:${White} Installing MariaDB $MARIADB_RELEASEVER.."
-dnf install -y expect mariadb-server 1>/dev/null
-systemctl enable --now mariadb 1>/dev/null
+dnf install -y expect mariadb-server &>/dev/null
+systemctl enable --now mariadb &>/dev/null
+echo -e "${Cyan}[INFO]:${White} Running MariaDB Secure Installation.."
 SECURE_MYSQL=$(expect -c '
 set timeout 10
 spawn mariadb-secure-installation
@@ -164,7 +115,16 @@ send "y\r"
 expect "Reload privilege tables now?"
 send "y\r"
 expect eof')
-echo "$SECURE_MYSQL"
+CHECK_MARIADB_INSTALL=$(echo $SECURE_MYSQL | grep 'All done')
+if [[ $CHECK_MARIADB_INSTALL == "All done"* ]]; then
+    echo -e "${Cyan}[INFO]:${White} MariaDB was set up successfully!"
+else
+    echo -e "${Red}[ERROR]:${White} MariaDB Installation returns !"
+    echo "$SECURE_MYSQL"
+fi
+
+
+
 dnf remove -y expect 1>/dev/null
 systemctl restart mariadb 1>/dev/null
 
@@ -198,9 +158,9 @@ PrivateTmp=true
 LimitNOFILE=32768" > /usr/lib/systemd/system/qbcore.service
 
 systemctl daemon-reload 1>/dev/null
-systemctl enable --now qbcore 1>/dev/null
+systemctl enable --now qbcore &>/dev/null
 echo -e "${Green}[NOTICE]:${White} Starting QBCore service.."
-echo -e "${Green}[INFO]:${White} Waiting for QBCore.."
+echo -e "${Green}[INFO]:${White} Waiting for qbcore.service to fetch setup items.."
 sleep 10
 txAdminToken=$(journalctl -u qbcore.service | grep '    ┃' | tail -2 | head -1 | rev | awk '{ print $2 }' | rev)
 txAdminUI=$(journalctl -u qbcore.service | grep '┃' | grep 'http' | tail -1 | cut -d '┃' -f2 | awk '{ print $1; }')
